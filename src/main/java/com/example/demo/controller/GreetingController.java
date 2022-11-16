54081afb041d8b0b68895ea2f16b8821e9212cfb
k-toshima <k-toshima@fores.jp>
2022-11-16 12:08:52 +0900

    プロジェクト作成

---
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -0,0 +1,25 @@
+package com.example.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping("/greeting")
+public class GreetingController {
+
+  @GetMapping("/hello")
+  public String hello(
+      @RequestParam(name = "name", required = false, defaultValue = "World") String name,
+      Model model) {
+    model.addAttribute("name", name);
+    return "greeting/hello";
+  }
+
+  @GetMapping("/morning")
+  public String morning(){
+    return "greeting/morning";
+  }
+}
