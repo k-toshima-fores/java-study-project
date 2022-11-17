d7f24a1541709c892bb1732bd3c348127699c578
k-toshima <k-toshima@fores.jp>
2022-11-17 19:48:47 +0900

    morningページの追加

--- src/main/java/com/example/demo/controller/GreetingController.java
+++ src/main/java/com/example/demo/controller/GreetingController.java
@@ -19,7 +19,10 @@ public class GreetingController {
   }
 
   @GetMapping("/morning")
-  public String morning(){
+  public String morning(
+      @RequestParam(name = "name", required = false, defaultValue = "World") String name,
+      Model model) {
+    model.addAttribute("name", name);
     return "greeting/morning";
   }
 }
